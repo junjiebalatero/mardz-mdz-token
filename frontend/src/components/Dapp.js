994201b8de958c20664960d6080f638ef439e309
junjiebalatero <junjiebalatero@gmail.com>
2022-10-04 00:30:12 +0800

    ok

--- frontend/src/components/Dapp.js
+++ frontend/src/components/Dapp.js
@@ -22,7 +22,7 @@ import { NoTokensMessage } from "./NoTokensMessage";
 // This is the Hardhat Network id that we set in our hardhat.config.js.
 // Here's a list of network ids https://docs.metamask.io/guide/ethereum-provider.html#properties
 // to use when deploying to other networks.
-const HARDHAT_NETWORK_ID = '3';
+const HARDHAT_NETWORK_ID = '5';
 
 // This is an error code that indicates that the user canceled a transaction
 const ERROR_CODE_TX_REJECTED_BY_USER = 4001;
--- hardhat.config.js
+++ hardhat.config.js
@@ -8,16 +8,16 @@ require('@nomiclabs/hardhat-ethers')
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const API_URL = "https://ropsten.infura.io/v3/041cd61ca4474f298e33d2e5ebeb2d00";
+const API_URL = "https://eth-goerli.g.alchemy.com/v2/xFNGEHiLw_pxLfkPEiFbQ5-8PeSKbDO4";
 const PRIVATE_KEY = "ed9ecbb246462ee1a7880279faeb06af336e3610778729105b91d2d5419c8e68"
 const PUBLIC_KEY = "0x36D22f8c3b043C716d903EE767D1337fAEDc3fA8";
 
 module.exports = {
   solidity: "0.8.9",
-  defaultNetwork: "ropsten",
+  defaultNetwork: "goerli",
   networks: {
     hardhat:{},
-    ropsten:{
+    goerli:{
       url: API_URL,
       accounts: [`0x${PRIVATE_KEY}`]
     }
