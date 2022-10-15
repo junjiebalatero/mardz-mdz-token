19d4adc9e3c152e1fe174008417f8362bf7bdcea
Margarito V. Balatero Jr <junjiebalatero@gmail.com>
2022-10-15 15:36:25 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,8 +8,8 @@ require('@nomiclabs/hardhat-ethers')
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const API_URL = "https://ropsten.infura.io/v3/041cd61ca4474f298e33d2e5ebeb2d00";
-const PRIVATE_KEY = "ed9ecbb246462ee1a7880279faeb06af336e3610778729105b91d2d5419c8e68"
+const API_URL = "https://eth-goerli.g.alchemy.com/v2/<your_api_key_here>";
+const PRIVATE_KEY = "6af336e3610778729105b91d2d5419c8e68"
 const PUBLIC_KEY = "0x36D22f8c3b043C716d903EE767D1337fAEDc3fA8";
 
 module.exports = {
